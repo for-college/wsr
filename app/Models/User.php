f021c4f92762f811d257d9cc7755f6b02abf5df8
scffs <khidey@inbox.ru>
2023-11-20 19:48:27 +0700

    work w/ auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,44 +2,56 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Model
 {
-  use HasApiTokens, HasFactory, Notifiable;
+  use HasFactory;
 
-  /**
-   * The attributes that are mass assignable.
-   *
-   * @var array<int, string>
-   */
   protected $fillable = [
     'name',
-    'email',
+    'surname',
+    'patronymic',
+    'login',
     'password',
+    'photo_file',
+    'role_id',
   ];
-
-  /**
-   * The attributes that should be hidden for serialization.
-   *
-   * @var array<int, string>
-   */
   protected $hidden = [
-    'password',
-    'remember_token',
+    'api_token',
+    'created_at',
+    'updated_at',
+    'role_id',
   ];
 
-  /**
-   * The attributes that should be cast.
-   *
-   * @var array<string, string>
-   */
-  protected $casts = [
-    'email_verified_at' => 'datetime',
-    'password' => 'hashed',
-  ];
+  public function role(): BelongsTo
+  {
+    return $this->belongsTo(Role::class);
+  }
+
+  /** Метод для генерации токена **/
+  public function generateToken(): string
+  {
+    $this->api_token = Hash::make(Str::random());
+    $this->save();
+
+    return $this->api_token;
+  }
+
+  /** Метод для проверки роли из списка ролей **/
+  public function hasRole($roles): bool
+  {
+    return in_array($this->role->code, $roles);
+  }
+
+  /** Метод для выхода из системы **/
+  public function logout(): void
+  {
+    $this->api_token = '';
+    $this->save();
+  }
 }
